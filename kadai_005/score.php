eb1dc4fe2a9b226f3801311e28bce616861d9b3d
kaito-web <2pop.seaside@gmail.com>
2023-06-15 10:51:17 +0900

    課題5の提出

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -9,7 +9,7 @@
 <?php
     $score = [80, 60, 55, 40, 100, 25, 80, 95, 30, 60];
     $add = $score[0] + $score[1] + $score[2] + $score[3]+ $score[4] + $score[5] + $score[6] + $score[7] + $score[8] + $score[9];
-    $commit = $add / 10;
+    $commit = $add / count($score);
     echo $commit;
 ?>
 
