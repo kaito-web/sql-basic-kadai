bfa9c7b143954693e75db20cd1631d5126818636
kaito-web <2pop.seaside@gmail.com>
2023-06-15 09:57:49 +0900

    first commit

---
+++ kadai_005/score.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="第5章課題提出">
+    <title>5章の提出</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ php-basic/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <!-- PHPの埋め込み -->
+    <?php
+    echo 'こんにちは！';
+    ?>
+</body>
+
+</html>
+
