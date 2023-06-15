29badf257bc3b9f848b630507c1974164597197d
kaito-web <2pop.seaside@gmail.com>
2023-06-15 10:46:56 +0900

    課題5の提出

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -6,6 +6,12 @@
     <title>5章の提出</title>
 </head>
 <body>
-    
+<?php
+    $score = [80, 60, 55, 40, 100, 25, 80, 95, 30, 60];
+    $add = $score[0] + $score[1] + $score[2] + $score[3]+ $score[4] + $score[5] + $score[6] + $score[7] + $score[8] + $score[9];
+    $commit = $add / 10;
+    echo $commit;
+?>
+
 </body>
 </html>
