c7de413a9d2bf50093449652b5823b9da46f6b81
kaito-web <2pop.seaside@gmail.com>
2023-06-15 11:53:50 +0900

    課題7の提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="課題7の提出">
+    <title>課題7の提出</title>
+</head>
+<body>
+<?php
+    $add = ['name' => 'onion', 'price' => 200 , 'weight' => 160];
+    print_r($add);
+?>
+</body>
+</html>
